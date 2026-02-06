e9a79bcb434d93b5922561e0c8657e2e83f5984b
lwb <lwb-2021@qq.com>
2026-02-06 10:58:41 +0800

    starship: configure prompt

--- home/programs/shell/starship.nix
+++ home/programs/shell/starship.nix
@@ -3,7 +3,19 @@
   programs.starship = {
     enable = true;
     settings = {
-
+      add_newline = true;
+      character = {
+        success_symbol = " [λ](bold green)";
+        error_symbol = " [λ](bold red)";
+        vimcmd_symbol = "[N](blue)[λ](bold green)";
+        vimcmd_visual_symbol = "[V](purple)[λ](bold green)";
+      };
+      cmd_duration = {
+        format = "[$duration]($style) ";
+      };
+      git_branch = {
+        format = "[$symbol$branch(:$remote_branch)]($style) ";
+      };
     };
   };
 }
