bfed957b7616ca989a1f099d7a08502bb696ab30
lwb <lwb-2021@qq.com>
2026-02-21 23:18:02 +0800

    noctalia-shell: fix screen locker

--- home/programs/desktop/noctalia/default.nix
+++ home/programs/desktop/noctalia/default.nix
@@ -7,6 +7,12 @@
     };
     settings = import ./settings.nix;
   };
+
+  services.screen-locker = {
+    enable = true;
+    lockCmd = "noctalia-shell ipc call lockScreen lock";
+  };
+
   systemd.user.services."noctalia-shell".Unit = {
     After = lib.mkForce [ "niri.service" ];
     Before = [ "graphical-session.target" ];
