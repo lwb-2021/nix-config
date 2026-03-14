4fc4a01c13f3b0f885b3e9cae20603d376c50542
lwb <lwb-2021@qq.com>
2026-03-14 22:22:21 +0800

    obsidian: add vaults setting

--- home/programs/text/note.nix
+++ home/programs/text/note.nix
@@ -1,11 +1,14 @@
-{ pkgs, ... }:
+{ config, ... }:
 {
   programs.obsidian = {
     enable = true;
+    defaultSettings = {
+    };
+    vaults."${config.home.homeDirectory}/Documents/Notes" = { };
   };
 
   data.sync.folders = [
-    "Documents/Obsidian"
+    "Documents/Notes"
   ];
   autostart.commands = [ "sleep 2s && obsidian" ];
   # "${pkgs.obsidian}/share/applications/obsidian.desktop"
