fbc404e5c596fb02b2db53c5baa4cbad19e041d3
lwb <lwb-2021@qq.com>
2026-03-20 21:57:46 +0800

    kitty: add some apperance settings

--- home/programs/basic/terminal/kitty.nix
+++ home/programs/basic/terminal/kitty.nix
@@ -12,8 +12,18 @@ in
   programs.kitty = {
     enable = true;
     settings = {
+      # Apperance
+      transparent_background_colors = "#1e1e2f";
       disable_ligatures = "never";
+      cursor_trail = "1";
+      cursor_trail_decay = "0.1 0.4";
+      pixel_scroll = "yes";
+
       shell = "${lib.getExe pkgs.fish} -i -c ${attach-to-tmux}";
+
+      initial_window_width = "179c"; # $COLUMNS
+      initial_window_height = "45c"; # $LINES
+
       confirm_os_window_close = "0";
     };
   };
