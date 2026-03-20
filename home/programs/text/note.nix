a29eac8d89b1f0e54104a70e810f221d98c618c5
lwb <lwb-2021@qq.com>
2026-03-20 21:57:33 +0800

    obsidian: remove vault config

--- home/programs/text/note.nix
+++ home/programs/text/note.nix
@@ -4,12 +4,14 @@
     enable = true;
     defaultSettings = {
     };
-    vaults."${config.home.homeDirectory}/Documents/Notes" = { };
+    vaults."${config.home.homeDirectory}/Documents/Notes" = {
+      enable = false;
+    };
   };
 
   data.sync.folders = [
     "Documents/Notes"
   ];
-  autostart.commands = [ "sleep 2s && obsidian" ];
+  autostart.commands = [ "sleep 2s && obsidian & " ];
   # "${pkgs.obsidian}/share/applications/obsidian.desktop"
 }
