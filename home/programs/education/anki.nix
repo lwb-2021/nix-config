678606adab9020275a8ce941a06b1acea66d4df7
lwb <lwb-2021@qq.com>
2026-04-04 09:33:47 +0800

    update system

--- home/programs/education/anki.nix
+++ home/programs/education/anki.nix
@@ -8,11 +8,13 @@
     language = "zh_CN";
     theme = "dark";
     style = "native";
-    sync = {
-      autoSync = true;
-      autoSyncMediaMinutes = 120;
-      username = "lwb-2021@qq.com";
-      keyFile = config.sops.secrets."anki/password".path;
+    profiles."User 1" = {
+      sync = {
+        autoSync = true;
+        autoSyncMediaMinutes = 120;
+        username = "lwb-2021@qq.com";
+        keyFile = config.sops.secrets."anki/password".path;
+      };
     };
   };
   data.local.directories = [ ".local/share/Anki2" ];
