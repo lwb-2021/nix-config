f463bedf721419461e756b788e19915ba9686dc9
lwb <lwb-2021@qq.com>
2026-04-04 09:33:58 +0800

    zswap: replace zram

--- os/desktop/optimize.nix
+++ os/desktop/optimize.nix
@@ -1,18 +1,29 @@
 { ... }:
 {
+  imports = [
+    # TODO https://github.com/NixOS/nixpkgs/pull/470366
+    (builtins.fetchurl {
+      url = "https://github.com/luochen1990/nixpkgs/raw/9bcddd22fd5454aaa97345975837518686dd658c/nixos/modules/system/boot/zswap.nix";
+      sha256 = "sha256:000m59zjk1qfwn9z09i0k1ajv6knv75cri91575ikqfg6r0fhs0g";
+    })
+  ];
   services.upower = {
     enable = true;
   };
-  zramSwap = {
+  # writebackDevice = "/dev/disk/by-uuid/ff5a2b4e-a5b6-4c72-a05e-2d41b02f1794";
+
+  boot.zswap = {
     enable = true;
-    writebackDevice = "/dev/disk/by-uuid/ff5a2b4e-a5b6-4c72-a05e-2d41b02f1794";
-    priority = 10;
-    memoryPercent = 200;
+    compressor = "zstd";
+    zpool = "zsmalloc";
+    maxPoolPercent = 25;
+    shrinkerEnabled = true;
   };
+
   boot.kernel.sysctl = {
     "kernel.sysrq" = 1;
 
-    "vm.swappiness" = 180;
+    "vm.swappiness" = 80;
     "vm.watermark_boost_factor" = 0;
     "vm.watermark_scale_factor" = 125;
     "vm.page-cluster" = 0;
