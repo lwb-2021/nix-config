912e4777c6f52f5aad2537f1edbaed5f0855c4d1
lwb <lwb-2021@qq.com>
2026-04-06 14:52:18 +0800

    tuigreet: add asterisks

--- os/universal/wayland.nix
+++ os/universal/wayland.nix
@@ -5,7 +5,7 @@
     useTextGreeter = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --user-menu --remember";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --user-menu --remember --asterisks";
         user = "greeter";
       };
     };
