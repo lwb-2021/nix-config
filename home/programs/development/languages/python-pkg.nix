3c1db0289edb743901acefdc4d38f404f4adefc2
lwb <lwb-2021@qq.com>
2026-05-02 11:34:06 +0800

    cybersecurity: add packages

--- home/programs/development/languages/python-pkg.nix
+++ home/programs/development/languages/python-pkg.nix
@@ -1,13 +1,26 @@
 { pkgs, ... }:
 pkgs.python3.withPackages (
   ps: with ps; [
+
+    # Data
     pandas
     matplotlib
     numpy
 
+    # HTML
     requests
     beautifulsoup4
     lxml
+
+    # Crypto
+    pycryptodome
+    gmpy2
+
+    # Web
+    scapy
+
+    # PWN
+    pwntools
 
     tabulate
 
