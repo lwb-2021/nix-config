80969172c4ffa9ddc0fbcc3aa792272ae03e98fa
lwb <lwb-2021@qq.com>
2026-05-10 16:21:26 +0800

    kitty: remove session choose

--- home/programs-old/basic/terminal/kitty.nix
+++ home/programs-old/basic/terminal/kitty.nix
@@ -1,11 +1,7 @@
 { lib, pkgs, ... }:
 let
-  attach-to-tmux = pkgs.writeScript "attach-to-tmux.fish" ''
-    #! /usr/bin/env fish
-    set -l fzf_height (if set -q FZF_TMUX_HEIGHT; echo $FZF_TMUX_HEIGHT; else; echo "40%"; end)
-    tmux ls -F '#{session_name}' | fzf --reverse --height $fzf_height --bind=enter:replace-query+print-query | read session
-    set -l session_name (if test -n "$session"; echo $session; else; echo "default"; end)
-    tmux attach -t $session_name; or tmux new -s $session_name
+  attach-to-tmux = pkgs.writeShellScript "attach-to-tmux.sh" ''
+    tmux attach -t default || tmux new -s default
   '';
 in
 {
@@ -28,5 +24,5 @@ in
     };
   };
   programs.tmux.terminal = "xterm-kitty";
-  wayland.terminal.exec = "kitty -1 ${lib.getExe pkgs.fish} -i -c ${attach-to-tmux}";
+  wayland.terminal.exec = "kitty -1 ${lib.getExe pkgs.bash} -i -c ${attach-to-tmux}";
 }
