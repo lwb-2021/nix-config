3c55f3c94fe1f8edec9526453562e7fac6279309
lwb <lwb-2021@qq.com>
2026-06-14 13:50:16 +0800

    nix: add resource limitation

--- os/desktop/system/nix.nix
+++ os/desktop/system/nix.nix
@@ -4,6 +4,14 @@
 {
   nix = {
     settings = {
+      connect-timeout = 5;
+      max-jobs = 8;
+
+      log-lines = 25;
+
+      max-free = (3000 * 1024 * 1024);
+      min-free = (512 * 1024 * 1024);
+
       experimental-features = [
         "nix-command"
         "flakes"
@@ -22,24 +30,52 @@
 
         "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       ];
+
+      auto-optimise-store = true;
     };
     extraOptions = ''
       keep-derivations = true
     '';
+
+    # From nix-community/servo
+    daemonCPUSchedPolicy = "batch";
+    daemonIOSchedClass = "idle";
+    daemonIOSchedPriority = 7;
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+
   };
+  systemd.services.nix-gc.serviceConfig = {
+    CPUSchedulingPolicy = "batch";
+    IOSchedulingClass = "idle";
+    IOSchedulingPriority = 7;
+  };
+  systemd.slices.anti-hungry.sliceConfig = {
+    CPUAccounting = true;
+    CPUQuota = "1200%";
 
-  boot.loader.grub.configurationLimit = 4;
+    IOWeight = 10;
 
-  # do garbage collection weekly to keep disk usage low
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
+    MemoryAccounting = true; # Allow to control with systemd-cgtop
+    MemoryWeight = 10;
+    MemoryHigh = "80%";
+    MemoryMax = "85%";
+    MemorySwapMax = "80%";
+    MemoryZSwapMax = "80%";
+  };
+  systemd.services.nix-daemon.serviceConfig = {
+    Slice = "anti-hungry.slice";
+
+    # Please kill nix builder when it eats up all my memory
+    OOMScoreAdjust = 500;
   };
 
   # Optimise storage
   # you can also optimise the store manually via:
   #    nix-store --optimise
   # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
-  nix.settings.auto-optimise-store = true;
 }
