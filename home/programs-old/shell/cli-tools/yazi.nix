7bf2aeadf7155673a01adcb6d7ebe9e888aa08d1
lwb <lwb-2021@qq.com>
2026-06-19 16:34:27 +0800

    yazi: add plugins

--- home/programs-old/shell/cli-tools/yazi.nix
+++ home/programs-old/shell/cli-tools/yazi.nix
@@ -1,8 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.yazi = {
     enable = true;
     shellWrapperName = "y";
+    plugins = with pkgs.yaziPlugins; {
+      drag = drag;
+    };
+    keymap = {
+      mgr.prepend_keymap = [
+        {
+          on = [ "<C-d>" ];
+          run = "plugin drag";
+          desc = "Drag Files";
+        }
+      ];
+    };
   };
-
+  home.packages = with pkgs; [
+    sshfs
+    ripdrag
+  ];
 }
