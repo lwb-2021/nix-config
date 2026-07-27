568cac1c9f101175cc4a62af3fce55b12447596d
lwb <lwb-2021@qq.com>
2026-07-27 17:59:48 +0800

    secrets: reformat

--- secrets/lwb/home.nix
+++ secrets/lwb/home.nix
@@ -4,17 +4,18 @@
     age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
     defaultSopsFile = ./files/home.yaml;
     secrets = {
-      "rclone/crypt/password" = { };
-      "rclone/jianguoyun/password" = { };
-      "rclone/onedrive/client-id" = { };
-      "rclone/onedrive/client-secret" = { };
-      "rclone/onedrive/token" = { };
       "restic/password" = { };
       "anki/password" = { };
       "vicinae/nix/github-token" = { };
 
       "ssh/keys/github" = { };
       "syncthing/passwords/backup" = { };
+
+      "rclone/crypt/password" = { };
+      "rclone/jianguoyun/password" = { };
+      "rclone/onedrive/client-id" = { };
+      "rclone/onedrive/client-secret" = { };
+      "rclone/onedrive/token" = { };
 
     };
   };
