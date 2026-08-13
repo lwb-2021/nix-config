3d2f3a409e9d2554337b6b6668a809d1d78e73a4
lwb <lwb-2021@qq.com>
2026-08-13 13:12:25 +0800

    weechat: drop

--- home/programs-old/network/chat.nix
+++ home/programs-old/network/chat.nix
@@ -1,15 +1,6 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    (weechat.override {
-      configure =
-        { availablePlugins, ... }:
-        {
-          scripts = with pkgs.weechatScripts; [
-            weechat-notify-send
-          ];
-        };
-    })
     fluffychat
   ];
   data.local.directories = [ ".local/share/chat.fluffy.fluffychat" ];
