8e0d98a8012eeaf0d3c04cfaf167f86db28ec91f
lwb <lwb-2021@qq.com>
2026-08-16 14:25:35 +0800

    update system

--- modules/universal/nixpkgs/overlays.nix
+++ modules/universal/nixpkgs/overlays.nix
@@ -26,20 +26,6 @@
   (final: prev: {
   })
   (final: prev: {
-    # TODO: https://github.com/NixOS/nixpkgs/pull/549747
-    frei0r = (
-      prev.callPackage (prev.fetchurl {
-        url = "https://github.com/nick-linux8/nixpkgs/raw/da903b7c11233ac14b4203a98cff6a6b2f4302e5/pkgs/by-name/fr/frei0r/package.nix";
-        hash = "sha256-frIW5EhPcCGYaW1E6vYMMqaUWexREHfcrqmYYB+FvEo=";
-      }) { }
-    );
-    gavl = (
-      prev.callPackage (prev.fetchurl {
-        url = "https://github.com/nick-linux8/nixpkgs/raw/da903b7c11233ac14b4203a98cff6a6b2f4302e5/pkgs/by-name/ga/gavl/package.nix";
-        hash = "sha256-lvo//8Qo9ik882r7u4lBZrtc9Q4IKqBCsCcmMRMmH8w=";
-      }) { }
-    );
-
     # For wemeet and others
     # TODO: https://github.com/niri-wm/niri/pull/1791
     niri = final.master.niri.overrideAttrs (old: {
